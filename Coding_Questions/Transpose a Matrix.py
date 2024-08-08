3245280b330ba9e6501f60a378ade230bec9aee3
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 20:11:48 +0530

    Create Transpose a Matrix.py

---
+++ Coding_Questions/Transpose a Matrix.py
@@ -0,0 +1,37 @@
+Example1 : Program to transpose a matrix using a nested loop
+
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+
+for r in result:
+   print(r)
+
+==>Output: 
+[12, 4, 3]
+[7, 5, 8]
+
+Example2: Matrix Transpose using Nested List Comprehension
+''' Program to transpose a matrix using list comprehension'''
+
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+
+for r in result:
+   print(r)
+
+==>Output: 
+[12, 4, 3]
+[7, 5, 8]
