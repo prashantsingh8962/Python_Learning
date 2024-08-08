47fabcbfc427723e0efb8a6dd2043b6302dad2f9
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-09 00:00:26 +0530

    Create Capitalize the First Character of a String.py

---
+++ Coding_Questions/Capitalize the First Character of a String.py
@@ -0,0 +1,17 @@
+Example 1: Using list slicing
+my_string = "programiz is Lit"
+
+print(my_string[0].upper() + my_string[1:])
+
+==>Output: Programiz is Lit
+
+
+
+Example 2: Using inbuilt method capitalize()
+my_string = "programiz is Lit"
+
+cap_string = my_string.capitalize()
+
+print(cap_string)
+
+==>Output: Programiz is Lit
