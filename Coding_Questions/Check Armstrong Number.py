ea21c0845af3bcb6ef40056c31f24fe5910e46ae
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 18:49:50 +0530

    Create Check Armstrong Number.py

---
+++ Coding_Questions/Check Armstrong Number.py
@@ -0,0 +1,53 @@
+Example1: Check Armstrong number (for 3 digits)
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+==>Output: Enter a number: 663
+663 is not an Armstrong number
+
+==>Output: Enter a number: 407
+407 is an Armstrong number
+
+Example2: Check Armstrong number of n digits
+num = 1634
+
+# Changed num variable to string, 
+# and calculated the length (number of digits)
+order = len(str(num))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+==>Output: 1634 is an Armstrong number
+
