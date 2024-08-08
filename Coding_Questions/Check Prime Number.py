a09aadfff5cbe263e0c12c890e5c40171928f040
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 18:27:02 +0530

    Create Check Prime Number.py

---
+++ Coding_Questions/Check Prime Number.py
@@ -0,0 +1,55 @@
+Example 1: Using a flag variable
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
+
+  ==>Output: 29 is a prime number
+
+Example 2: Using a for...else statement
+num = 407
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+
+==>Output: 407 is not a prime number
+11 times 37 is 407
