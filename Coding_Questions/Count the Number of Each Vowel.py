a234021c4d1aaf446232855e944edbda0c3609a9
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 20:26:52 +0530

    Create Count the Number of Each Vowel.py

---
+++ Coding_Questions/Count the Number of Each Vowel.py
@@ -0,0 +1,36 @@
+Example1: Program to count the number of each vowels
+
+# string of vowels
+vowels = 'aeiou'
+
+ip_str = 'Hello, have you tried our tutorial section yet?'
+
+# make it suitable for caseless comparisions
+ip_str = ip_str.casefold()
+
+# make a dictionary with each vowel a key and value 0
+count = {}.fromkeys(vowels,0)
+
+# count the vowels
+for char in ip_str:
+   if char in count:
+       count[char] += 1
+
+print(count)
+
+==>Output: {'o': 5, 'i': 3, 'a': 2, 'e': 5, 'u': 3}
+
+Example2: Using a list and a dictionary comprehension
+# Using dictionary and list comprehension
+
+ip_str = 'Hello, have you tried our tutorial section yet?'
+
+# make it suitable for caseless comparisions
+ip_str = ip_str.casefold()
+
+# count the vowels
+count = {x:sum([1 for char in ip_str if char == x]) for x in 'aeiou'}
+
+print(count)
+
+==>Output: {'o': 5, 'i': 3, 'a': 2, 'e': 5, 'u': 3}
