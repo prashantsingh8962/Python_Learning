1837c351fc5efccc51493ab0d0d1eb70c16d7a4c
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-09 00:04:29 +0530

    Create Create a Countdown Timer.py

---
+++ Coding_Questions/Create a Countdown Timer.py
@@ -0,0 +1,22 @@
+#Countdown time in Python
+import time
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+    print("stop")
+
+countdown(5)
+
+==>Output:
+00:05
+00:04
+00:03
+00:02
+00:01
+stop
