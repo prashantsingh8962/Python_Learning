707a38f27e01cae563f2d0042208c54c016346c9
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-09 00:06:33 +0530

    Create Count the Number of Occurrence of a Character in String.py

---
+++ Coding_Questions/Count the Number of Occurrence of a Character in String.py
@@ -0,0 +1,23 @@
+Example 1: Using a for loop
+count = 0
+
+my_string = "Programiz"
+my_char = "r"
+
+for i in my_string:
+    if i == my_char:
+        count += 1
+
+print(count)
+
+==>Output: 2
+
+
+
+Example 2: Using method count()
+my_string = "Programiz"
+my_char = "r"
+
+print(my_string.count(my_char))
+
+==>Output: 2
