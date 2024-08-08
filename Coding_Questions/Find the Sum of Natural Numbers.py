bd4075313f3cd66e7873c002460bac86e858c0e1
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 18:52:07 +0530

    Create Find the Sum of Natural Numbers.py

---
+++ Coding_Questions/Find the Sum of Natural Numbers.py
@@ -0,0 +1,15 @@
+# Sum of natural numbers up to num
+
+num = 16
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
+
+==>Output: The sum is 136
