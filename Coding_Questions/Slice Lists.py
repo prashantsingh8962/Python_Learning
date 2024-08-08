9ce1cb5f97cf903cc726bb4ceac608257f77f8d7
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 21:37:17 +0530

    Create Slice Lists.py

---
+++ Coding_Questions/Slice Lists.py
@@ -0,0 +1,58 @@
+#Get all the Items
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:])
+
+==>Output: [1, 2, 3, 4, 5]
+
+
+#Get all the Items After a Specific Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:])
+
+==>Output:[3, 4, 5]
+
+
+#Get all the Items Before a Specific Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:2])
+
+==>Output: [1, 2]
+
+
+#Get all the Items from One Position to Another Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:4])
+
+==>Output: [3, 4]
+
+
+
+#Get the Items at Specified Intervals
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[::2])
+
+==>Output: [1, 3, 5]
+
+
+#negative intervals
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[::-2])
+
+==>Output: [5, 3, 1]
+
+
+# interval between a range
+my_list = [1, 2, 3, 4, 5]
+
+==>Output: [2, 4]
+
+
+print(my_list[1:4:2])
+
+
