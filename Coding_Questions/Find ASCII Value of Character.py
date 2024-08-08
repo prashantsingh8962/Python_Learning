a8299a007fcf92d0fdd5a07a293c12b024b73f30
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 18:57:33 +0530

    Create Find ASCII Value of Character.py

---
+++ Coding_Questions/Find ASCII Value of Character.py
@@ -0,0 +1,14 @@
+Example1: Program to find the ASCII value of the given character
+
+c = 'p'
+print("The ASCII value of '" + c + "' is", ord(c))
+
+==>Output: The ASCII value of 'p' is 112
+
+Example2: Modify the code above to get characters from their corresponding ASCII values using the chr() function as shown below.
+>>> chr(65)
+'A'
+>>> chr(120)
+'x'
+>>> chr(ord('S') + 1)
+'T'
