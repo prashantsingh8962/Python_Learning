8a4e1fccbd74a90f5d853ccd5056e508ab176035
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 20:43:03 +0530

    Create Merge Two Dictionaries.py

---
+++ Coding_Questions/Merge Two Dictionaries.py
@@ -0,0 +1,28 @@
+Example 1: Using the | Operator
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print(dict_1 | dict_2)
+
+==>Output: {1: 'a', 2: 'c', 4: 'd'}
+
+Example 2: Using the ** Operator
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
+
+==>Output: {1: 'a', 2: 'c', 4: 'd'}
+
+Example 3: Using copy() and update()
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+dict_3 = dict_2.copy()
+dict_3.update(dict_1)
+
+print(dict_3)
+
+==>Output: {2: 'b', 4: 'd', 1: 'a'}
+
+
