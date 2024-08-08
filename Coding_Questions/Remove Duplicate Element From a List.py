b9e4f9620baee3d6e95817895929091429c99db5
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-09 00:08:48 +0530

    Create Remove Duplicate Element From a List.py

---
+++ Coding_Questions/Remove Duplicate Element From a List.py
@@ -0,0 +1,20 @@
+Example 1: Using set()
+list_1 = [1, 2, 1, 4, 6]
+
+print(list(set(list_1)))
+
+==>Output: [1, 2, 4, 6]
+
+
+Example 2: Remove the items that are duplicated in two lists
+list_1 = [1, 2, 1, 4, 6]
+list_2 = [7, 8, 2, 1]
+
+print(list(set(list_1) ^ set(list_2)))
+
+==>Output: [4, 6, 7, 8]
+
+
+
+
+
